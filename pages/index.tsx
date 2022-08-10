b0de9daba7a7ec07516d63c51fe42e4122951f88
mshahanwaz <mohdshahanwaz55@gmail.com>
2022-08-10 20:07:22 +0530

    fix: removed warnings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { GetServerSideProps, NextPage } from 'next'
+import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 import Button from 'components/Button'
