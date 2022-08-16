7d3209e16a73a1c88835c4be7cd14a15f5c91a33
Shubham Yadav <shubhamy2015@gmail.com>
2022-08-16 20:27:23 +0100

    update setup scripts

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 cp .env.example .env.local
 echo "\nCMC_API_KEY=$1" >> .env.local # replace $123 with your Coinmarketcap API key
 echo "API_ENDPOINT=https://vscout.io" >> .env.local # replace $123 with your API endpoint to fetch node data
+mkdir data && echo '{}' > data/avax.json
